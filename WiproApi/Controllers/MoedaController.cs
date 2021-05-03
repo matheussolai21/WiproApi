2c68673e2341bbd13ef19573e9625cb53ccdc7a5
matheussolai21 <matheussolai99@gmail.com>
2021-05-03 00:26:41 -0300

    Adicionar arquivos de projeto.

---
+++ WiproApi/Controllers/MoedaController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using WiproApi.Models;
+
+namespace WiproApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoedaController : ControllerBase
+    {
+        Stack<DadosMoeda> Fila;
+         
+        [HttpPost]
+        public void AddItemFila(Stack<DadosMoeda> dados)
+        {
+            Fila = new Stack<DadosMoeda>();
+
+            foreach (var item in dados)
+            {
+                Fila.Push(item);
+            }
+           
+            HttpContext.Session.SetString("SessionFila", JsonConvert.SerializeObject(Fila));
+        }
+
+        [HttpGet]
+        public IActionResult GetItemFila()
+        {
+            Fila  = JsonConvert.DeserializeObject<Stack<DadosMoeda>> 
+              (HttpContext.Session.GetString("SessionFila"));
+
+            if (Fila.Any())
+            {
+                var ultimoFila = Fila.Peek();
+                Fila.Pop();
+
+               
+                HttpContext.Session.SetString("SessionFila", JsonConvert.SerializeObject(Fila));
+
+                return Ok(ultimoFila);
+            }
+
+            return NotFound("Fila Vazia");
+        }
+    }
+}
---
+++ WiproApi/Models/DadosMoeda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WiproApi.Models
+{
+    public class DadosMoeda
+    {
+        public string Moeda { get; set; }
+        public DateTime Data_Inicio { get; set; }
+        public DateTime Data_Fim { get; set; }
+    }
+}
